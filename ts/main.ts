1fdffaefff3fe4d3a347b5bc48e9b210b32d021d
Uziel Ferreira do Prado <pessoal.ziera@gmail.com>
2023-09-24 21:42:09 -0300

    working prototype

---
+++ ts/main.ts
@@ -0,0 +1,36 @@
+(<HTMLInputElement>document.getElementById("calc")).addEventListener("click", calc);
+
+function calc(): void {
+    var nota1 = parse((<HTMLInputElement>document.getElementById("nota1")).value, "Nota 1");
+    var nota2 = parse((<HTMLInputElement>document.getElementById("nota2")).value, "Nota 2");
+
+    var trim_1e2 = nota1 + nota2;
+    var falta = (600 - 3 * trim_1e2) / 4
+
+    console.log(falta);
+    
+    if (falta <= 0) {
+        escrever(`Você já passou nessa disciplina!`);
+        return;
+    }
+
+    if (falta > 100) {
+        escrever(`Infelizmente você já rodou nessa matéria, pois precisa de ${(Math.floor(falta + 0.5) / 10).toFixed(1)} pontos.`);
+        return;
+    }
+
+    escrever(`Te faltam ${(Math.floor(falta + 0.5) / 10).toFixed(1)} pontos.`);
+}
+
+function parse(str: string, nota: string): number {
+    if (str.length == 1 || str == "10") return 10*parseInt(str);
+    if (str.length !== 3) {escrever(`${nota} invalida.`);throw "Erro" + nota;}
+
+    var n = str.split(",").join("").split(".").join("");
+
+    return parseInt(n);
+}
+
+function escrever(msg: string): void {
+    (<HTMLSpanElement>document.getElementById("res")).innerText = msg;
+}
