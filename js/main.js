1d896728b5f4d12fbc48e7c4a0fc368e16fa058c
Uziel Ferreira do Prado <pessoal.ziera@gmail.com>
2023-09-25 21:13:53 -0300

    Some updates, better colors and more responsive

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
 "use strict";
-document.getElementById("calc").addEventListener("click", calc);
+var nota1_e = document.getElementById("nota1");
+var nota2_e = document.getElementById("nota2");
+nota1_e.addEventListener("input", calc);
+nota2_e.addEventListener("input", calc);
+const COLORS = {
+    Error: "#f20808",
+    Reproved: "#a20202",
+    Approved: "#08f208",
+    Needing: "#08f2f2"
+};
 function calc() {
-    var nota1 = parse(document.getElementById("nota1").value, "Nota 1");
-    var nota2 = parse(document.getElementById("nota2").value, "Nota 2");
-    var trim_1e2 = nota1 + nota2;
-    var falta = (600 - 3 * trim_1e2) / 4;
-    console.log(falta);
-    if (falta <= 0) {
-        escrever(`Você já passou nessa disciplina!`);
-        return;
+    var nota1 = nota1_e.value;
+    var nota2 = nota2_e.value;
+    if (nota1 == "10")
+        nota1 = "100";
+    if (nota2 == "10")
+        nota2 = "100";
+    if (nota1 == "" || nota1.match(/[a-z]/i) != null || nota1.length == 2 || nota1.length > 3) {
+        return escrever("A primeira nota é inválida. Verifique se digitou corretamente!", COLORS.Error);
     }
-    if (falta > 100) {
-        escrever(`Infelizmente você já rodou nessa matéria, pois precisa de ${(Math.floor(falta + 0.5) / 10).toFixed(1)} pontos.`);
-        return;
+    if (nota2 == "" || nota2.match(/[a-z]/i) != null || nota2.length == 2 || nota2.length > 3) {
+        return escrever("A segunda nota é invalida. Verifique se digitou corretamente!", COLORS.Error);
     }
-    escrever(`Te faltam ${(Math.floor(falta + 0.5) / 10).toFixed(1)} pontos.`);
+    var n1 = parseInt(nota1.split(",").join("").split(".").join(""));
+    var n2 = parseInt(nota2.split(",").join("").split(".").join(""));
+    if (n1 > 100)
+        return escrever("Parece que a primeira nota é maior do que deveria.", COLORS.Error);
+    if (n2 > 100)
+        return escrever("Parece que a segunda nota é maior do que deveria.", COLORS.Error);
+    n1 *= nota1.length == 1 ? 10 : 1;
+    n2 *= nota2.length == 1 ? 10 : 1;
+    var soma = n1 + n2;
+    var falta = (600 - 3 * soma) / 4;
+    if (falta < 0)
+        return escrever("Parece que alguma nota é maior do que deveria.", COLORS.Error);
+    if (falta > 100)
+        return escrever("Infelizmente você já foi reprovado nessa disciplina.", COLORS.Reproved);
+    var fst = ((falta + 0.5) / 10).toFixed(1);
+    if (falta == 0)
+        return escrever("Você já foi aprovado nessa disciplina!", COLORS.Approved);
+    escrever(`Lhe faltam ${fst} pontos para ser aprovado nessa disciplina!`, COLORS.Needing);
 }
-function parse(str, nota) {
-    if (str.length == 1 || str == "10")
-        return 10 * parseInt(str);
-    if (str.length !== 3) {
-        escrever(`${nota} invalida.`);
-        throw "Erro" + nota;
-    }
-    var n = str.split(",").join("").split(".").join("");
-    return parseInt(n);
-}
-function escrever(msg) {
+function escrever(msg, color) {
+    document.getElementsByClassName("wrapper-output")[0].style.backgroundColor = color;
     document.getElementById("res").innerText = msg;
 }
